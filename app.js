d81830782658c4d636643c0912bb980deca99b40
mswed <mswed@beapot.com>
2023-09-14 21:29:22 -0400

    css changes, hidden lists

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const lists = document.querySelector('#lists')
 const now = document.querySelector('#now')
 const next = document.querySelector('#next')
 const future = document.querySelector('#future')
-
+toggleLists()
 
 // New Todo Listener
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    if (!todo.value) {
+        alert('Ahhh... Enter some in the todo field?')
+        return
+    }
     let selectedOption = when.options[when.options.selectedIndex].text;
     if (selectedOption === 'Now') {
         targetList = now;
@@ -29,6 +33,7 @@ form.addEventListener('submit', function(e) {
     newTodo.prepend(checkbox)
     newTodo.appendChild(removeBtn)
     targetList.appendChild(newTodo)
+    toggleLists()
 
 });
 
@@ -37,19 +42,36 @@ lists.addEventListener('click', function(e) {
     let text = ''
     let cb = ''
     if (e.target.tagName === 'LI') {
+        // We clicked a list. Find the checkbox.
         text = e.target;
         cb = text.children[0];
     } else if (e.target.tagName === 'INPUT') {
+        // We clicked a checkbox. Find the list item.
         cb = e.target
         text = e.target.parentElement
     } else if (e.target.tagName === 'BUTTON'){
+        // We clicked a button. Remove the list item. 
         e.target.parentElement.remove()
         return
     } else {
+        // Ignore any other type of element
         return
     }
 
     text.classList.toggle('completed')
-    cb.checked = !cb.checked
+    text.classList.contains('completed') ? cb.checked = true : cb.checked = false;
+    toggleLists()
     
 })
+
+function toggleLists() {
+    for (let list of [now, next, future]) {
+        console.dir(list.parentElement)
+        console.log('>>>', list.parentElement.style.display)
+        if (list.children.length > 0) {
+            list.parentElement.style.display = 'block'
+        } else {
+            list.parentElement.style.display = 'none'
+        }
+    }
+}
