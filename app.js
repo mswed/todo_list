c97e0525fe039693a798b475ac97c346ea0d41bd
mswed <mswed@beapot.com>
2023-09-14 22:29:31 -0400

    todos are saved and loaded

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const lists = document.querySelector('#lists')
 const now = document.querySelector('#now')
 const next = document.querySelector('#next')
 const future = document.querySelector('#future')
+loadTodos()
 toggleLists()
 
 // New Todo Listener
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     if (!todo.value) {
         alert('Ahhh... Enter some in the todo field?')
@@ -25,20 +26,24 @@ form.addEventListener('submit', function(e) {
     }
 
     const newTodo = document.createElement('li')
+    const todoId = Math.random() * Math.random()
     const checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
     const removeBtn = document.createElement('button')
     removeBtn.innerText = 'X'
     newTodo.innerHTML = todo.value + '&nbsp&nbsp&nbsp'
+    newTodo.dataset.id = todoId
     newTodo.prepend(checkbox)
     newTodo.appendChild(removeBtn)
+
     targetList.appendChild(newTodo)
     toggleLists()
+    saveTodos()
 
 });
 
 // Existing Todo Listener
-lists.addEventListener('click', function(e) {
+lists.addEventListener('click', function (e) {
     let text = ''
     let cb = ''
     if (e.target.tagName === 'LI') {
@@ -49,9 +54,11 @@ lists.addEventListener('click', function(e) {
         // We clicked a checkbox. Find the list item.
         cb = e.target
         text = e.target.parentElement
-    } else if (e.target.tagName === 'BUTTON'){
+    } else if (e.target.tagName === 'BUTTON') {
         // We clicked a button. Remove the list item. 
         e.target.parentElement.remove()
+        saveTodos()
+        toggleLists()
         return
     } else {
         // Ignore any other type of element
@@ -61,17 +68,57 @@ lists.addEventListener('click', function(e) {
     text.classList.toggle('completed')
     text.classList.contains('completed') ? cb.checked = true : cb.checked = false;
     toggleLists()
-    
+
 })
 
 function toggleLists() {
     for (let list of [now, next, future]) {
-        console.dir(list.parentElement)
-        console.log('>>>', list.parentElement.style.display)
         if (list.children.length > 0) {
             list.parentElement.style.display = 'block'
         } else {
             list.parentElement.style.display = 'none'
+        }
+    }
+}
+
+function saveTodos() {
+    for (let list of [now, next, future]) {
+        let todos = []
+        window.localStorage.setItem(list.id, [])
+        for (let todo of list.children) {
+            todos.push(todo.innerHTML)
+            console.log(todos)
+            window.localStorage.setItem(list.id, todos)
+        }
+    }
+}
+
+function loadTodos() {
+    for (let list of ['now', 'next', 'future']) {
+        todos = localStorage.getItem(list)
+        if (todos) {
+            todos = todos.split(',')
+
+            console.log(todos)
+            if (list === 'now') {
+                targetList = now
+            } else if (list === 'next') {
+                targetList = next
+            } else {
+                targetList = future
+            }
+
+
+            for (let todo of todos) {
+                console.log('>>>>>', todo)
+                // console.log(JSON.parse(todos))
+                newTodo = document.createElement('li')
+                newTodo.innerHTML = todo
+                console.log('>>>>', newTodo)
+                targetList.appendChild(newTodo)
+                toggleLists()
+
+            }
         }
     }
 }
